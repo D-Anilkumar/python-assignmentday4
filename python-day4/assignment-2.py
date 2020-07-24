af2fac1bc59377bbdb1dbbeaf168cdbb98f3f96f
D-Anilkumar <68576263+D-Anilkumar@users.noreply.github.com>
2020-07-24 08:34:25 +0530

    Add files via upload

---
+++ python-day4/assignment-2.py
@@ -0,0 +1,36 @@
+
+
+# Python code for implementation of isupper() 
+  
+# checking for uppercase characters 
+string = 'LETSUPGRADE'
+print(string.isupper()) 
+  
+string = 'letsupgrade'
+print(string.isupper()) 
+
+
+# Python code for implementation of isupper() 
+  
+# checking for lowercase characters 
+string = 'letsupgrade'
+print(string.islower()) 
+  
+string = 'letsupgrade'
+print(string.islower()) 
+
+
+# Python code to demonstrate working of  
+# isupper() and islower() 
+str = "LetsUgrade"
+str1 = "let"
+  
+# checking if all characters in str are upper cased 
+if str.isupper() : 
+       print ("All characters in str are upper cased") 
+else : print ("All characters in str are not upper cased") 
+  
+# checking if all characters in str1 are lower cased 
+if str1.islower() : 
+       print ("All characters in str1 are lower cased") 
+else : print ("All characters in str1 are not lower cased")
